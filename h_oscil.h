d992ab5c3558fd926951177d06d72a82bbf2bde8
S, Gautam <cyclicintegral@gmail.com>
2018-02-04 12:10:54 +0530

    2D Dimensional Features

--- h_oscil.h
+++ h_oscil.h
@@ -5,16 +5,18 @@
 // A classical harmonic oscillator
 class CHarmonic_Oscillator {
 public:
-	double t; // Absolute time since creation.
 	double m; // Rest mass
 	double k; // Hooke's Constant
 	double v; // Velocity
+	double theta; // Angle with the horizontal
 	double x; // Current position (equilibria at 0)
+	double y; 
 	double T; // Kinetic Energy
 	double V; // PE
-	double U; // Total
+	double U; // Totaldouble t; // Implementation variable, frequency of calling.
+	double l_0x, l_0y; // Natural length from pivot
 	
-	CHarmonic_Oscillator(double m, double k, double i); // i = impulse
+	CHarmonic_Oscillator(double m, double k, double l_0x, double l_0y, double i, double theta); // i = impulse
 	void Update(void); // Updates the oscillator
 	
 };
